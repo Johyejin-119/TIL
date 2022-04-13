8cea890f16754c912965bb5e5aa29a30c73ef4ef
Johyejin-119 <hyejinjo0101@gmail.com>
2022-04-13 15:07:59 +0900

    docs: 'test3' Connecting the Collar from Eclipse

--- src/gitTest/GitTest1.java
+++ src/gitTest/GitTest1.java
@@ -3,7 +3,9 @@ package gitTest;
 public class GitTest1 {
 	public static void main(String[] args) {
 		System.out.println("how to git in Eclipse, test1");
-		System.out.println("how to git in Eclipse, test2");
+		System.out.println("how to git in Eclipse, test2"); // commit
+		System.out.println("how to git in Eclipse, test3"); // commit and push
+
 
 	}
 }
