c401f87f681799d7da267a54dbe3f4d14626d416
Johyejin-119 <hyejinjo0101@gmail.com>
2022-04-13 15:07:04 +0900

    docs: 'test2' Connecting the git from Eclipse

--- src/gitTest/GitTest1.java
+++ src/gitTest/GitTest1.java
@@ -3,5 +3,7 @@ package gitTest;
 public class GitTest1 {
 	public static void main(String[] args) {
 		System.out.println("how to git in Eclipse, test1");
+		System.out.println("how to git in Eclipse, test2");
+
 	}
 }
