fe5f0ac8d6d38ca679341ea84f79bf24bb712bfb
Johyejin-119 <hyejinjo0101@gmail.com>
2022-04-20 22:21:45 +0900

    feat: Implementing a Generic

---
+++ src/ch07/GenericPrinterTest.java
@@ -0,0 +1,42 @@
+package ch07;
+
+public class GenericPrinterTest {
+
+/*  Generic은 번거롭게 Object 타입으로 형변환 하지 않아도 된다.
+ * 	
+	GenericPrinter<Powder>
+	 
+	컴파일될 때 GenericPrinter.java 파일의 <T>가 
+	Powder로 변경된다
+*/
+	public static void main(String[] args) {
+
+		Powder powder = new Powder();
+		GenericPrinter<Powder> powderPrinter = new GenericPrinter<>();
+		powderPrinter.setMaterial(powder);
+		
+		Powder p = powderPrinter.getMaterial();
+		System.out.println(powderPrinter.toString());
+		
+		Plastic plastic = new Plastic();
+		GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<>();
+		plasticPrinter.setMaterial(plastic);
+		
+		Plastic pl = plasticPrinter.getMaterial();
+		System.out.println(plasticPrinter.toString());
+		
+		/* 예전 방식
+		
+		Powder powder = new Powder();
+		GenericPrinter powderPrinter = new GenericPrinter<>(); //<Powder>없이 Object타입으로 지정
+		powderPrinter.setMaterial(powder);
+		
+		Powder p = (Powder)powderPrinter.getMaterial(); // 형 변환 해줘야함
+		System.out.println(powderPrinter.toString());
+		
+		*/
+		
+
+	}
+
+}
---
+++ src/ch07/TreeDPrinter3.java
@@ -0,0 +1,18 @@
+package ch07;
+
+public class TreeDPrinter3 {
+
+	private Object material;
+	
+	public Object getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(Object material) {
+		this.material = material;
+	}
+
+	public String toString() {
+		return "재료는 Plasticf 입니다";
+	}
+}
