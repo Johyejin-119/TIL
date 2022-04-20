2616eaa98d5d3f2f5228b023aeddc6be5d132ee2
Johyejin-119 <hyejinjo0101@gmail.com>
2022-04-20 22:26:54 +0900

    feat: how to use ArrayList and how to use API

---
+++ src/ch10/MemberArrayList.java
@@ -0,0 +1,41 @@
+package ch10;
+
+import java.util.ArrayList;
+
+public class MemberArrayList {
+
+	private ArrayList<Member> arrayList;
+	
+	public MemberArrayList() {
+		arrayList = new ArrayList<>();
+	}	
+
+	public MemberArrayList(int size) {
+		arrayList = new ArrayList<>(size);
+	}	
+	
+	public void addMember(Member member) {
+		arrayList.add(member);
+	}
+	
+	public boolean removeMember(int memberId) {
+		for(int i = 0; i<arrayList.size(); i++) {
+			Member member = arrayList.get(i);
+			
+			int tempId = member.getMemberId();
+			if(tempId == memberId) { // 기존 member의 id값과 삭제할 member의 id값이 같으면 remove
+				arrayList.remove(i);
+				return true;
+			}
+		}
+		System.out.println(memberId + "가 존재하지 않습니다.");
+		return false;
+	}
+	
+	public void showAllMember() {
+		for(Member member : arrayList) {
+			System.out.println(member);
+		}
+		System.out.println();
+	}
+}
