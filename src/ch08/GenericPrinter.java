678c9d7dffe46c1bf9475a198518fea1a0a3783b
Johyejin-119 <hyejinjo0101@gmail.com>
2022-04-20 22:23:09 +0900

    feat: Implementing Generic's <T> extends Class

---
+++ src/ch08/GenericPrinter.java
@@ -0,0 +1,18 @@
+package ch08;
+
+public class GenericPrinter<T extends Material> { // T에 대한 제한
+
+	private T material;
+
+	public T getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(T material) {
+		this.material = material;
+	}
+	
+	public String toString() {
+		return material.toString();
+	}
+}
