4973e42c1cac26c3633e4d3e81657c00aeae0800
Johyejin-119 <hyejinjo0101@gmail.com>
2022-05-10 13:13:13 +0900

    # Creating Proxy Pattern
    ## Implementing the Browser's Cache Functionality

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,6 +1,9 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
@@ -22,6 +25,7 @@ public class Main {
       */
 
      // Adapter Pattern
+     /*
         HairDryer hairDryer = new HairDryer();
         connect(hairDryer); // 출력 : 헤어 들라이기 110v on
 
@@ -34,8 +38,19 @@ public class Main {
         AirConditioner airConditioner = new AirConditioner();
         Electronic110V airAdapter = new SocketAdapter(airConditioner);
         connect(airAdapter); // 출력 : 에어컨 220v on
+      */
 
-
+     // Proxy
+        // Browser browser = new Browser("www.naver.com");
+        // browser.show(); 를 할때마다, url 이 새롭게 계속 할당(loading)되므로 비효율적
+        
+        // Proxy 를 통해서, Cache(캐시) 를 만들어 url 저장
+        IBrowser browser1 = new BrowserProxy("www.naver.com");
+        browser1.show(); // 한번만 loading
+        browser1.show(); // 동일한 url 은 cache 사용
+        browser1.show();
+        browser1.show();
+        browser1.show();
     }
 
     // Adapter 콘센트 동작
