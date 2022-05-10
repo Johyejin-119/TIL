cb02f5878b6a389b2fc7291396b5dab9e2652af0
Johyejin-119 <hyejinjo0101@gmail.com>
2022-05-10 14:36:49 +0900

    # Creating Decorator Pattern
    ## To add a price by grade

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -2,6 +2,7 @@ package com.company.design;
 
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
+import com.company.design.decorator.*;
 import com.company.design.proxy.Browser;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
@@ -9,12 +10,13 @@ import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
 
+import javax.swing.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     public static void main(String[] args) {
 
-     // Singleton Pattern
+     // Singleton
      /* AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
 
@@ -27,7 +29,7 @@ public class Main {
         System.out.println(aClient.equals(bClient));
       */
 
-     // Adapter Pattern
+     // Adapter
      /*
         HairDryer hairDryer = new HairDryer();
         connect(hairDryer); // 출력 : 헤어 들라이기 110v on
@@ -44,18 +46,18 @@ public class Main {
       */
 
      // Proxy
+     /*
         // Browser browser = new Browser("www.naver.com");
         // browser.show(); 를 할때마다, url 이 새롭게 계속 할당(loading)되므로 비효율적
         
         // Proxy 를 통해서, Cache(캐시) 를 만들어 url 저장
-        /*
         IBrowser browser1 = new BrowserProxy("www.naver.com");
         browser1.show(); // 한번만 loading
         browser1.show(); // 동일한 url 은 cache 사용
         browser1.show();
         browser1.show();
         browser1.show();
-        */
+
 
         // Proxy 를 통해서, Aop 기능 구현
         // 시간 method 는 동시성 문제때문에 atomic 사용
@@ -78,6 +80,24 @@ public class Main {
 
         aopBrowser.show();
         System.out.println("loading time2 : " + end.get()); // cache 를 사용해서 0초
+      */
+
+     // Decorator
+        ICar audi = new Audi(1000);
+        audi.showPrice();
+
+        // 기존 가격에서 등급을 하나씩 추가하는 decorator
+        // a3
+        ICar audi3 = new A3(audi, "A3");
+        audi3.showPrice(); // 1000
+
+        // a4
+        ICar audi4 = new A4(audi, "A4");
+        audi4.showPrice(); // 2000
+
+        // a5
+        ICar audi5 = new A5(audi, "A5");
+        audi5.showPrice(); // 3000
     }
 
     // Adapter 콘센트 동작
