2ee9c3d86e9317cff7d00315041c75e390650f6a
Johyejin-119 <hyejinjo0101@gmail.com>
2022-05-10 13:43:08 +0900

    # Creating Proxy Pattern
    ## Implementing the Browser's Cache Functionality
    ## Implementing the Aop Functionality

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,12 +1,15 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
 import com.company.design.proxy.Browser;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     public static void main(String[] args) {
@@ -45,12 +48,36 @@ public class Main {
         // browser.show(); 를 할때마다, url 이 새롭게 계속 할당(loading)되므로 비효율적
         
         // Proxy 를 통해서, Cache(캐시) 를 만들어 url 저장
+        /*
         IBrowser browser1 = new BrowserProxy("www.naver.com");
         browser1.show(); // 한번만 loading
         browser1.show(); // 동일한 url 은 cache 사용
         browser1.show();
         browser1.show();
         browser1.show();
+        */
+
+        // Proxy 를 통해서, Aop 기능 구현
+        // 시간 method 는 동시성 문제때문에 atomic 사용
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+        // Aop 기능 람다식으로 표현
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                () -> {
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis()); // 시작 시간
+                },
+                () -> {
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get()); // 끝난 시간(실행 시간) = 현재 시간 - 시작 시간
+                }
+        );
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get()); // 1.5초 + 실행 시간
+
+        aopBrowser.show();
+        System.out.println("loading time2 : " + end.get()); // cache 를 사용해서 0초
     }
 
     // Adapter 콘센트 동작
