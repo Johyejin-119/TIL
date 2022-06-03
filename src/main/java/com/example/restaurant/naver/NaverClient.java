665962b6522c9640cd8bb7f22d110e36d6c6ddc3
Johyejin-119 <hyejinjo0101@gmail.com>
2022-06-03 12:03:33 +0900

    feat: Implementing the Naver API Connection and DTO
    ## local search, image search

---
+++ src/main/java/com/example/restaurant/naver/NaverClient.java
@@ -0,0 +1,85 @@
+package com.example.restaurant.naver;
+
+import com.example.restaurant.naver.dto.SearchImageReq;
+import com.example.restaurant.naver.dto.SearchImageRes;
+import com.example.restaurant.naver.dto.SearchLocalReq;
+import com.example.restaurant.naver.dto.SearchLocalRes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class NaverClient {
+
+    @Value("${naver.client.id}")
+    private String naverClientId;
+
+    @Value("${naver.client.secret}")
+    private String naverClinetSecret;
+
+    @Value("${naver.url.search.local}")
+    private String naverLocalSearchUrl;
+
+    @Value("${naver.url.search.image}")
+    private String naverImageSearchUrl;
+
+    // 장소 검색
+    public SearchLocalRes searchLocal(SearchLocalReq searchLocalReq) {
+        var uri = UriComponentsBuilder.fromUriString(naverLocalSearchUrl)
+                .queryParams(searchLocalReq.toMultiValueMap())
+                .build()
+                .encode()
+                .toUri();
+
+        var headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id", naverClientId);
+        headers.set("X-Naver-Client-Secret", naverClinetSecret);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // 요청
+        var httpEntity = new HttpEntity<>(headers);
+        var responseType = new ParameterizedTypeReference<SearchLocalRes>(){};
+
+        var responseEntity = new RestTemplate().exchange(
+                uri,
+                HttpMethod.GET,
+                httpEntity,
+                responseType
+        );
+        return responseEntity.getBody();
+    }
+
+    // 이미지 검색
+    public SearchImageRes searchImage(SearchImageReq searchImageReq) {
+        var uri = UriComponentsBuilder.fromUriString(naverImageSearchUrl)
+                .queryParams(searchImageReq.toMultiValueMap())
+                .build()
+                .encode()
+                .toUri();
+
+        var headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id", naverClientId);
+        headers.set("X-Naver-Client-Secret", naverClinetSecret);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // 요청
+        var httpEntity = new HttpEntity<>(headers);
+        var responseType = new ParameterizedTypeReference<SearchImageRes>(){};
+
+        var responseEntity = new RestTemplate().exchange(
+                uri,
+                HttpMethod.GET,
+                httpEntity,
+                responseType
+        );
+        return responseEntity.getBody();
+    }
+
+
+}
--- src/main/java/com/example/restaurant/wishlist/repository/WishListRepository.java
+++ src/main/java/com/example/restaurant/wishlist/repository/WishListRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository // 데이터 저장하는 곳(Repository)
 public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
     // MemoryDbRepositoryAbstract 클래스의 <T> 는 WishListEntity 로 대체되어 동작
+
+
+
 }
