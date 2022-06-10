b815e44ff16346b258064fc2bb3478fb010c70ad
Johyejin-119 <hyejinjo0101@gmail.com>
2022-06-10 10:24:01 +0900

    feat: Compare the two numbers
    ## Using 'if~else if~else'
    ## step2
    ## 1330

---
+++ src/main/java/step2/ComparisonTwoNum.java
@@ -0,0 +1,25 @@
+package step2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 1330
+public class ComparisonTwoNum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
