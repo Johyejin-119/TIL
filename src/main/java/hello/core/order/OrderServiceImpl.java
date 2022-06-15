dc21cec5e8fe0ac1c96d865f7084071225943b1e
Johyejin-119 <hyejinjo0101@gmail.com>
2022-06-15 16:49:52 +0900

    feat: Studying at the Inflearn #2
    ## Spring Core Principles - Basic Part
    ## Apply DI(Dependency Injection)

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,15 +2,20 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); // 회원 찾기
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 고정 할인
+    private final MemberRepository memberRepository; // 회원 찾기
+    private final DiscountPolicy discountPolicy; // 고정 할인
 
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     // 단일 책임 원칙 O
     @Override
