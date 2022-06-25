56c38414faedf626e23ee7e2897ba3e4bea21ec5
Johyejin-119 <hyejinjo0101@gmail.com>
2022-06-25 11:03:09 +0900

    feat: Adders that exit at 00 input
    ## while, StringTokenizer
    ## step3
    ## 10952

---
+++ src/main/java/step3/APlusBTest2.java
@@ -0,0 +1,24 @@
+package step3;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+// 10952
+public class APlusBTest2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        while (a != 0 || b != 0) {
+            bw.write((a + b) + "\n");
+            st = new StringTokenizer(br.readLine(), " ");
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+        }
+        bw.close();
+    }
+}
