bb4567747eeeadd4f1e39cfab152047ce997593c
Johyejin-119 <hyejinjo0101@gmail.com>
2022-07-13 17:52:09 +0900

    feat: Implementing a Mini Shop Practice
    ## Analyze requirements and map basic

---
+++ jpabook/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        // 트랜잭션 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        // 우리가 실행할 코드 작성
+        try {
+
+            tx.commit();
+        } catch (Exception e) {
+            em.close();
+        }
+        emf.close();
+    }
+}
