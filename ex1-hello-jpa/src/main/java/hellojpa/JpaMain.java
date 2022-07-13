76d9586ce3f1d17b4e9bb0e63e0996a5e9c4f6e9
Johyejin-119 <hyejinjo0101@gmail.com>
2022-07-13 12:19:28 +0900

    feat: Studying Entity Mapping
    ## Mapping Objects to Tables
    ## Mapping Fields to Columns

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -10,20 +10,13 @@ public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        
+
         // 트랜잭션 시작
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         // 우리가 실행할 코드 작성
         try {
-            //Member member = em.find(Member.class, 1L);
-            //member.setName("HelloJPA");
-            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(2)
-                    .setMaxResults(5)
-                    .getResultList(); // 대상은 객체
-
             tx.commit();
         } catch (Exception e) {
             em.close();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,26 +1,36 @@
 package hellojpa;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
     @Id
     private Long id;
-    private String name;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "name") // 데이터베이스 컬럼명은 name
+    private String username;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private Integer age;
 
-    public String getName() {
-        return name;
-    }
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    // 생성 일자
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    private LocalDate testLocalDate; // 년 월
+    private LocalDateTime localDateTime; // 년 월 일
+
+    // 수정 일자
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    public Member(){}
+
 }
