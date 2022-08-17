9d5a4fd14adbf49d704c69743b8259291196c8c3
Johyejin-119 <hyejinjo0101@gmail.com>
2022-08-18 00:15:43 +0900

    feat: Setting Preferences

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class) // Spring 관련 테스트라는 것을 JUnit에게 알려줌
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false) // 데이터 db에 저장
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); // true // 같은 영속성 컨텍스트안에서 id 값이 같다면? 같은 entity 이다.
+
+    }
+
+}
