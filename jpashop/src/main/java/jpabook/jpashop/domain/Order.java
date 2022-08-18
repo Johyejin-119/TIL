6a7e8b41e86123886cd9b70e6798cbfe3e2eb877
Johyejin-119 <hyejinjo0101@gmail.com>
2022-08-19 00:31:59 +0900

    feat: To modify an entity design
    1. Do not use @Setter
    2. Relationship is LAZY LOADING
    3. Initialize Collection in field
    4. Create an Association Convenience Method

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -16,14 +16,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // OrderItem 각 엔티티들 연결지어 동작
     private List<OrderItem> orderItems = new ArrayList<>(); // 주문 목록
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id1")
     private Delivery delivery;
 
@@ -31,4 +31,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
