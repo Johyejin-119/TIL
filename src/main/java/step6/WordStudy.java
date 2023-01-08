4718a3dd5a1529067d3e700ac31c87997501d96c
Johyejin-119 <hyejinjo0101@gmail.com>
2023-01-08 17:52:03 +0900

    feat: 단어 공부
    ## step6 1157
    ## for문 이용
    - 입력 받은 알파벳이 count[]배열의 index(알파벳순)의 값을 증가

---
+++ src/main/java/step6/WordStudy.java
@@ -0,0 +1,36 @@
+package step6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class WordStudy {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            if ('A' <= s.charAt(i) && s.charAt(i) <= 'Z') {
+                count[s.charAt(i) - 'A']++;
+            } else {
+                count[s.charAt(i) - 'a']++;
+            }
+        }
+
+        int max = -1;
+        char ch = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                ch = (char) (i + 65);
+            } else if (count[i] == max) {
+                ch = '?';
+            }
+        }
+        System.out.println(ch);
+
+    }
+}
