d679de7f11d87eb31dd6c8438d758811302f11d8
Johyejin-119 <hyejinjo0101@gmail.com>
2023-01-08 18:18:36 +0900

    refactor: 안쓰는 import 제거

--- src/main/java/step6/WordStudy.java
+++ src/main/java/step6/WordStudy.java
@@ -1,8 +1,6 @@
 package step6;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class WordStudy {
     public static void main(String[] args) throws IOException {
