b89d6b1027468fca663d1d01b1e8845cc415d8b0
Johyejin-119 <hyejinjo0101@gmail.com>
2023-01-18 15:53:16 +0900

    feat: 손익분기점
    ## step7 1712
    ## 2100000000 오버플로 문제 해결
    ## 수식화하여 while문 대신 if문 사용

--- src/main/java/step6/BreakEvenPoint.java
+++ src/main/java/step6/BreakEvenPoint.java
@@ -3,25 +3,21 @@ package step6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 1712
 public class BreakEvenPoint {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
 
-        String[] s = br.readLine().split(" ");
-        int A = Integer.parseInt(s[0]);
-        int B = Integer.parseInt(s[1]);
-        int C = Integer.parseInt(s[2]);
-        int count = 0;
-
-        while ( (C*count) <= (A + (B * count))) {
-            count++;
-        }
-        if ((C*count) > (A + (B * count))){
+        int A = Integer.parseInt(str.nextToken());
+        int B = Integer.parseInt(str.nextToken());
+        int C = Integer.parseInt(str.nextToken());
+        if (C <= B) {
             System.out.println(-1);
         } else {
-            System.out.println(count);
+            System.out.println(A / (C-B) + 1);
         }
     }
 }
