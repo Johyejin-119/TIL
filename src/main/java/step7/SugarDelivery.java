5f157d258adcd74ce7cfd145def0af2d31d1761f
Johyejin-119 <hyejinjo0101@gmail.com>
2023-01-23 23:35:46 +0900

    feat: 설탕 배달
    ## step7 2839
    ## N%5가 3,1인 경우 1를 더함
    ## N%5가 4,2인 경우 2를 더함
    ## 4와 7일때는 -1 출력

--- src/main/java/step7/SugarDelivery.java
+++ src/main/java/step7/SugarDelivery.java
@@ -9,10 +9,17 @@ public class SugarDelivery {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        if ((N%5) == 0) {
-            System.out.println(N/5);
-        } else {
-            System.out.println((N/5)+((N%5)%3+1));
+        if (N == 4 || N == 7) {
+            System.out.println(-1);
+        }
+        else if (N % 5 == 0) {
+            System.out.println(N / 5);
+        }
+        else if (N % 5 == 1 || N % 5 == 3) {
+            System.out.println((N / 5) + 1);
+        }
+        else if (N % 5 == 2 || N % 5 == 4) {
+            System.out.println((N / 5) + 2);
         }
     }
 }
