0b317da026d935cc0b1aa680e42d1dea9c923a67
Johyejin-119 <hyejinjo0101@gmail.com>
2023-01-23 23:51:14 +0900

    feat: 설탕 배달
    ## step7 2839
    ## N/5, N-3을 while문으로 반복

--- src/main/java/step7/SugarDelivery.java
+++ src/main/java/step7/SugarDelivery.java
@@ -9,17 +9,22 @@ public class SugarDelivery {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        if (N == 4 || N == 7) {
-            System.out.println(-1);
-        }
-        else if (N % 5 == 0) {
-            System.out.println(N / 5);
-        }
-        else if (N % 5 == 1 || N % 5 == 3) {
-            System.out.println((N / 5) + 1);
-        }
-        else if (N % 5 == 2 || N % 5 == 4) {
-            System.out.println((N / 5) + 2);
+        int cnt = 0;
+
+        while (true) {
+            if (N%5 == 0) {
+                cnt += N/5;
+                System.out.println(cnt);
+                break;
+            }
+            else {
+                N -= 3;
+                cnt++;
+            }
+            if (N < 0) {
+                System.out.println(-1);
+                break;
+            }
         }
     }
 }
