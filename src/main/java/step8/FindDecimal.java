3cf42fe76cdbb6cb8d7f63f58a26779c5012a245
Johyejin-119 <hyejinjo0101@gmail.com>
2023-01-25 14:33:37 +0900

    feat: 소수 찾기
    ## step8 1978
    ## 소수 N은 '2부터 (N-1)까지' 나눠떨어지는 수가 존재하지 않음
    ## 나눠떨어지는 수 && N==1를 제외한 나머지를 출력

--- src/main/java/step8/FindDecimal.java
+++ src/main/java/step8/FindDecimal.java
@@ -1,5 +1,28 @@
 package step8;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 // 1978
 public class FindDecimal {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+        int cnt;
+        int decimal = 0;
+
+        for (int i = 0; i < N; i++) {
+            int S = Integer.parseInt(str.nextToken());
+            cnt = 0;
+            for (int j = 2; j < S; j++) {
+                if (S%j == 0) cnt++;
+            }
+            if (cnt == 0 && S != 1) decimal++;
+        }
+        System.out.println(decimal);
+    }
 }
