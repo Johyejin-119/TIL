ffdd0b58d149eb7a89108150f238e99a5033e11f
Johyejin-119 <hyejinjo0101@gmail.com>
2023-01-26 23:37:00 +0900

    feat: 소수
    ## step8 2581
    ## list.stream()으로 합계 및 최솟값 구함
    ## 소수 구하는 코드는 '1978 소수찾기'의 코드 재활용

---
+++ src/main/java/step8/Decimal.java
@@ -0,0 +1,31 @@
+package step8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+// 2581
+public class Decimal {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> decimal = new ArrayList<>();
+        int cntDecimal;
+
+        for (int i = M; i <= N; i++) {
+            cntDecimal = 0;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) cntDecimal++;
+            }
+            if (cntDecimal == 0 && i != 1) decimal.add(i);
+        }
+        if (decimal.isEmpty()) {
+            System.out.println("-1");
+        } else {
+            System.out.println(decimal.stream().mapToInt(Integer::intValue).sum() + "\n" + decimal.stream().mapToInt(x -> x).min().getAsInt());
+        }
+    }
+}
