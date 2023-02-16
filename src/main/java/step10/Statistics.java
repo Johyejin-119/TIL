6c527366ac09c8bf7ae87d8b191ac6ee5cc78348
Johyejin-119 <hyejinjo0101@gmail.com>
2023-02-16 15:26:20 +0900

    feat: 통계학 중 평균값 구하기
    ## step10 2108
    ## stream.average()로 평균값 구함
    ## Math.round로 반올림값 구함

---
+++ src/main/java/step10/Statistics.java
@@ -0,0 +1,32 @@
+package step10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// 2108
+public class Statistics {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<Integer> numbers = new ArrayList<>();
+        StringBuilder sb =  new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            numbers.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(numbers);
+
+        // average
+        double average = numbers.stream().mapToInt(Integer::intValue).average().orElse(0);
+        sb.append((int)Math.round(average));
+
+
+
+
+    }
+}
