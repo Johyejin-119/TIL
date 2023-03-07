eb9f839ef31800ea30da0fde534b24d36ce0c06f
Johyejin-119 <hyejinjo0101@gmail.com>
2023-03-07 16:46:11 +0900

    feat: 통계학 중 최빈값, 범위 구하기
    ## step10 2108
    ## Counting 정렬 사용

--- src/main/java/step10/Statistics.java
+++ src/main/java/step10/Statistics.java
@@ -3,33 +3,59 @@ package step10;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
 
 // 2108
 public class Statistics {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
 
-        List<Integer> numbers = new ArrayList<>();
-        StringBuilder sb =  new StringBuilder();
+        int[] arr = new int[8001];
 
-        for (int i = 0; i < N; i++) {
-            numbers.add(Integer.parseInt(br.readLine()));
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int median = 10000;
+        int mode = 10000;
+
+        for(int i = 0; i < N; i++) {
+            int value = Integer.parseInt(br.readLine());
+            sum += value;
+            arr[value + 4000]++;
+
+            if(max < value)  max = value;
+            if(min > value)  min = value;
         }
-        Collections.sort(numbers);
-
-        // average
-        double average = numbers.stream().mapToInt(Integer::intValue).average().orElse(0);
-        sb.append((int)Math.round(average));
-
-        // center
-        int center = numbers.get((N / 2));
-        sb.append(center);
 
 
+        int count = 0;
+        int mode_max = 0;
 
+        boolean flag = false;
+
+        for(int i = min + 4000; i <= max + 4000; i++) {
+
+            if(arr[i] > 0) {
+
+                if(count < (N + 1) / 2) {
+                    count += arr[i];
+                    median = i - 4000;
+                }
+
+                if(mode_max < arr[i]) {
+                    mode_max = arr[i];
+                    mode = i - 4000;
+                    flag = true;
+                }
+                else if(mode_max == arr[i] && flag == true) {
+                    mode = i - 4000;
+                    flag = false;
+                }
+            }
+        }
+
+        System.out.println((int)Math.round((double)sum / N) + "\n" + median + "\n" + mode + "\n" + (max - min));
     }
 }
