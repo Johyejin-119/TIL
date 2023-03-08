bbfd997181bd332eac8189d68cbf12cf9248729d
Johyejin-119 <hyejinjo0101@gmail.com>
2023-03-08 23:55:35 +0900

    feat: 소트인사이드
    ## step10 1427
    ## Counting 알고리즘과 charAt()으로 오름차순 정렬

---
+++ src/main/java/step10/SortInside.java
@@ -0,0 +1,25 @@
+package step10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 1427
+public class SortInside {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+        int[] num = new int[1000000000];
+
+        for (int i = 0; i < N.length(); i++) {
+            num[Integer.parseInt(String.valueOf(N.charAt(i)))]++;
+        }
+        br.close();
+
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] > 0) {
+                System.out.print(i);
+            }
+        }
+    }
+}
