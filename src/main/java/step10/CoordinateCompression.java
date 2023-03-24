a924a3b0842ce7f1abf3e20888d9fff595a51205
Johyejin-119 <hyejinjo0101@gmail.com>
2023-03-24 21:24:21 +0900

    feat: 오름차순 정렬
    ## step 정렬 18870 - 좌표 압축

---
+++ src/main/java/step10/CoordinateCompression.java
@@ -0,0 +1,27 @@
+package step10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+// 18870
+public class CoordinateCompression {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] originCC = new int[N];
+        int[] sortedCC = new int[N];
+        HashMap<Integer, Integer> rankCC = new HashMap<>();
+
+        StringTokenizer str;
+        for (int i = 0; i < N; i++) {
+            str = new StringTokenizer(br.readLine(), " ");
+            originCC[i] = sortedCC[i] = Integer.parseInt(str.nextToken());
+        }
+        Arrays.sort(sortedCC);
+
+    }
+}
