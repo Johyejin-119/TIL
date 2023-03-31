18088e0417d3518176748aa055b249c7883cbe56
Johyejin-119 <hyejinjo0101@gmail.com>
2023-03-31 22:52:36 +0900

    feat: 3중 for문으로 전체 카드를 순회
    ## step 브루트 포스 2798 - 블랙잭
    ## 카드가 m보다 작거나 같을 때만 수식 계산

--- src/main/java/step11/BlackJack.java
+++ src/main/java/step11/BlackJack.java
@@ -22,12 +22,24 @@ public class BlackJack {
         br.close();
 
         int result = sumCard(card, N, M);
-        System.out.println(result);
+        System.out.print(result);
     }
 
     private static int sumCard(int[] card, int n, int m) {
         int res = 0;
+        for (int i = 0; i < n - 2; i++) {
+            if (card[i] > m) continue;
+
+            for (int j = i + 1; j < n - 1 ; j++) {
+                if (card[i]+card[j] > m) continue;
+
+                for (int k = j + 2; k < n; k++) {
+                    int temp = card[i] + card[j] + card[k];
+                    if (temp == m) return temp;
+                    if (temp < m && temp > res) res = temp;
+                }
+            }
+        }
         return res;
     }
-
 }
