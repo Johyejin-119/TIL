629c3d40cc7eb25d25d2f628c2658feebba6464f
Johyejin-119 <hyejinjo0101@gmail.com>
2023-04-06 23:07:00 +0900

    feat: 입력값 변수 6개 선언
    ## step 수학은 비대면강의입니다 19532
    ## BufferReader, StringTokenizer

---
+++ src/main/java/step11/MathOnlineClass.java
@@ -0,0 +1,21 @@
+package step11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 19532
+public class MathOnlineClass {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(str.nextToken());
+        int b = Integer.parseInt(str.nextToken());
+        int c = Integer.parseInt(str.nextToken());
+        int d = Integer.parseInt(str.nextToken());
+        int e = Integer.parseInt(str.nextToken());
+        int f = Integer.parseInt(str.nextToken());
+
+    }
+}
