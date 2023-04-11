bba91463c4ad5a5971b7f7226406cccb90e11d96
Johyejin-119 <hyejinjo0101@gmail.com>
2023-04-11 23:54:29 +0900

    feat: 인접한 두 칸을 비교(count), 최소 count 구하기
    ## 체스판 다시 칠하기 - 1018
    ## repaintingCount()

--- src/main/java/step11/RepaintingChessboard.java
+++ src/main/java/step11/RepaintingChessboard.java
@@ -7,13 +7,14 @@ import java.util.StringTokenizer;
 
 // 1018
 public class RepaintingChessboard {
+    public static boolean[][] check;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer str = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(str.nextToken());
         int M = Integer.parseInt(str.nextToken());
 
-        boolean[][] check = new boolean[N][M];
+        check = new boolean[N][M];
         for (int i = 0; i < N; i++) {
             String row = br.readLine();
             for (int j = 0; j < M; j++) {
@@ -21,9 +22,33 @@ public class RepaintingChessboard {
             }
         }
 
+        int result = 0;
+        for (int i = 0; i < N-7; i++) {
+            for (int j = 0; j < M-7; j++) {
+                result = repaintingCount(i, j);
+            }
+        }
+        System.out.println(result);
     }
 
-    private static void checkWhiteOrBlack(boolean[][] check, int i, String row, int j) {
+    public static int repaintingCount(int row, int col) {
+        int count = 0;
+        int min = 64;
+        boolean firstColor = check[row][col];
+
+        for (int i = row; i < row + 8; i++) {
+            for (int j = col; j < col + 8; j++) {
+                if (firstColor != check[i][j]) count++;
+                firstColor = !firstColor;
+            }
+            firstColor = !firstColor;
+        }
+        count = Math.min(count, (64 - count));
+        min = Math.min(min, count);
+        return min;
+    }
+
+    public static void checkWhiteOrBlack(boolean[][] check, int i, String row, int j) {
         if (row.charAt(j) == 'W') { // white
             check[i][j] = true;
         }
