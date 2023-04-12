204ec57c243d77425006c310bd5ebcc481a203b0
Johyejin-119 <hyejinjo0101@gmail.com>
2023-04-12 23:22:33 +0900

    feat: 정수, 정수 배열 전역 변수 선언

---
+++ src/main/java/HanteoGlobal/Q2.java
@@ -0,0 +1,11 @@
+package HanteoGlobal;
+
+public class Q2 {
+    static int[] coins;
+    static int count = 0;
+    static int n;
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
