622a08edb9a857918956008d6003040702228a65
Johyejin-119 <hyejinjo0101@gmail.com>
2023-04-12 23:33:03 +0900

    feat: 입출력 구현
    ## BufferReader, StringTokenizer
    - coins[]: 통화 수를 나타내는 정수 배열
    - sum: 합계
    - n: coins[] 배열 크기
    - count: 합계가 되는 경우의 수의 총 개수

--- src/main/java/HanteoGlobal/Q2.java
+++ src/main/java/HanteoGlobal/Q2.java
@@ -1,11 +1,32 @@
 package HanteoGlobal;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Q2 {
     static int[] coins;
     static int count = 0;
     static int n;
 
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "= ,[]{}");
 
+        int sum = 0;
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            if (token.equals("sum")) {
+                sum = Integer.parseInt(st.nextToken());
+            } else if (token.equals("coins")) {
+                n = st.countTokens();
+                coins = new int[n];
+                for (int i = 0; i < n; i++) {
+                    coins[i] = Integer.parseInt(st.nextToken());
+                }
+            }
+        }
+        System.out.println(count);
     }
+
 }
