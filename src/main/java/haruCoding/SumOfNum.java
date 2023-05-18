beb9f494435d54ebfdafa4f04015f8a6089d2d76
Johyejin-119 <hyejinjo0101@gmail.com>
2023-05-18 20:13:55 +0900

    refactor: 클래스 파일명 변

--- src/main/java/haruCoding/SumOfNum.java
+++ src/main/java/haruCoding/SumOfNum.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 // 11720
-public class sumOfNum {
+public class SumOfNum {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String N = br.readLine();
