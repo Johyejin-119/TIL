bb3f49b959b39e8b99e4baa0f779a6a3a78f30d4
Johyejin-119 <hyejinjo0101@gmail.com>
2023-05-21 20:54:40 +0900

    feat: 입력값, 최댓값 비교 후 평균 구하기
    ## 평균 구하기 - 1546
    ## 평균 구하는 수식 간소화

---
+++ src/main/java/haruCoding/AverageScore.java
@@ -0,0 +1,23 @@
+package haruCoding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 1546
+public class AverageScore {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+        long sum = 0, max = 0;
+        for (int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(str.nextToken());
+            if (temp > max) max = temp;
+            sum += temp;
+        }
+        System.out.println(sum * 100.0 / max / N);
+    }
+}
