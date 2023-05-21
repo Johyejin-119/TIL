4dda9bca2cf220fc21f1334d304a6d4b3b7d2e44
Johyejin-119 <hyejinjo0101@gmail.com>
2023-05-21 22:13:04 +0900

    feat: 변수 및 배열 선언
    ## 평균 구하기 - 11659

---
+++ src/main/java/haruCoding/PrefixSum.java
@@ -0,0 +1,18 @@
+package haruCoding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 11659
+public class PrefixSum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(str.nextToken());
+        int M = Integer.parseInt(str.nextToken());
+        long[] S = new long[N + 1];
+
+    }
+}
