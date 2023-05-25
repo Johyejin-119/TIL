39fad84eaeaaea75f36e55656c9ddd9803736774
Johyejin-119 <hyejinjo0101@gmail.com>
2023-05-26 00:11:20 +0900

    feat: 증가되는 스택 값과 입력받은 수열 값을 순차적으로 비교 연산
    ## 백준 1874
    ## 수열 값 >= 스택 값인 경우, 같아질 때까지 push 연산
    ## 수열 값 < 스택 값인 경우, pop 연산

--- src/main/java/haruCoding/CreateSequenceStack.java
+++ src/main/java/haruCoding/CreateSequenceStack.java
@@ -3,6 +3,7 @@ package haruCoding;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 // 1874
 public class CreateSequenceStack {
@@ -12,7 +13,23 @@ public class CreateSequenceStack {
         int[] seq = new int[N];
         for (int i = 0; i < N; i++) {
             seq[i] = Integer.parseInt(br.readLine());
+        }
 
+        Stack<Integer> stack = new Stack<>();
+        int s_value = 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < seq.length; i++) {
+            if (seq[i] >= s_value) {
+                while (seq[i] == s_value) {
+                    stack.push(s_value++);
+                    sb.append("+\n");
+                }
+                stack.pop();
+                sb.append("-\n");
+            }
+            else {
+                stack.pop();
+            }
         }
 
     }
