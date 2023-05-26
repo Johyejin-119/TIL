c9424e9aea81d241c7c122a37c761c99e1f350df
Johyejin-119 <hyejinjo0101@gmail.com>
2023-05-27 00:12:50 +0900

    feat: 변수 및 우선순위 큐 선언
    ## 백준 11286

---
+++ src/main/java/haruCoding/AbsoluteValueHeap.java
@@ -0,0 +1,15 @@
+package haruCoding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+// 11286
+public class AbsoluteValueHeap {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pQueue = new PriorityQueue<>();
+    }
+}
