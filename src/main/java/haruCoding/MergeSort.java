9a8402735956c7ad1a6e8d9a27cc81b9b9be2aef
Johyejin-119 <hyejinjo0101@gmail.com>
2023-06-06 00:22:25 +0900

    feat: 중앙 값(mid)을 기준으로 데이터 분할
    ## 백준 2751
    ## mergeSort(): 재귀 호출로, 데이터 집합이 가장 작을 때까지 분할
    ## merge(): 분할 후, 병합 수행

--- src/main/java/haruCoding/MergeSort.java
+++ src/main/java/haruCoding/MergeSort.java
@@ -6,18 +6,31 @@ import java.io.InputStreamReader;
 
 // 2751
 public class MergeSort {
+
+    static int[] arrMerge;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] mergeSort = new int[N];
+        arrMerge = new int[N];
 
         for (int i = 0; i < N; i++) {
-            mergeSort[i] = Integer.parseInt(br.readLine());
+            arrMerge[i] = Integer.parseInt(br.readLine());
         }
+        mergeSort(arrMerge, 0, arrMerge.length-1);
+    }
 
+    private static void mergeSort(int[] arrMerge, int start, int end) {
+        int mid = 0;
+        if (start < end) {
+            mid = (start + end) / 2;
+            mergeSort(arrMerge, start, mid);
+            mergeSort(arrMerge, mid + 1, end);
+            merge(arrMerge, start, mid, end);
+        }
+    }
 
-
+    private static void merge(int[] arrMerge, int start, int mid, int end) {
 
     }
 }
