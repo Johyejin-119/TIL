68973bb3cdb1b45b5781751d9cd30209247aaebb
Johyejin-119 <hyejinjo0101@gmail.com>
2023-06-08 01:12:05 +0900

    feat: 큐 초기화
    ## 백준 2751

--- src/main/java/haruCoding/RadixSort.java
+++ src/main/java/haruCoding/RadixSort.java
@@ -3,6 +3,7 @@ package haruCoding;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 import java.util.Queue;
 
 // 2751
@@ -24,6 +25,10 @@ public class RadixSort {
 
     private static void radixSort(int[] arrRadix, int textSize) {
         Queue<Integer>[] queue = new Queue[10];
+        for (int i = 0; i < queue.length; i++) {
+            queue[i] = new LinkedList<>();
+        }
+
         int digit = 10; // 자릿수를 구하기 위해 나눠질 변수
         while(textSize != 0) {
 
