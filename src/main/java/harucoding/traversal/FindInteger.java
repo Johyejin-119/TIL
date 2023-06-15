64fbf9f2ea18440e22e7f8444c7e3e5ba5cd515b
Johyejin-119 <hyejinjo0101@gmail.com>
2023-06-15 17:16:45 +0900

    feat: 비교 기준이 될 변수 및 배열 선언하기
    ## 백준 1920

---
+++ src/main/java/harucoding/traversal/FindInteger.java
@@ -0,0 +1,21 @@
+package harucoding.traversal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 1920
+public class FindInteger {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");;
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(str.nextToken());
+        }
+
+    }
+}
