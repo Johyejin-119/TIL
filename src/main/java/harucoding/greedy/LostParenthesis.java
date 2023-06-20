1f455fb31ddba9878b03f70ddcfa3fff663aab8e
Johyejin-119 <hyejinjo0101@gmail.com>
2023-06-20 20:18:16 +0900

    feat: 변수 및 배열 입력받고, -연산 기준으로 split()
    ## 백준 1541

---
+++ src/main/java/harucoding/greedy/LostParenthesis.java
@@ -0,0 +1,16 @@
+package harucoding.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 1541
+public class LostParenthesis {
+    static int result = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String ex = br.readLine();
+
+        String[] input = ex.split("-");
+    }
+}
