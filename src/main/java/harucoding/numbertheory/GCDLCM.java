354d5c29f46d32b789894c76ed46ea214a58b2d6
Johyejin-119 <hyejinjo0101@gmail.com>
2023-06-22 16:33:43 +0900

    feat: 두 수의 범위 입력받기
    ## 백준 2609

---
+++ src/main/java/harucoding/numbertheory/GCDLCM.java
@@ -0,0 +1,17 @@
+package harucoding.numbertheory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 2609
+public class GCDLCM {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(str.nextToken());
+        int B = Integer.parseInt(str.nextToken());
+    }
+}
