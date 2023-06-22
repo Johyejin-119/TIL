a7f2e1e4a1e214c6df48ac9e83a4616273a407f9
Johyejin-119 <hyejinjo0101@gmail.com>
2023-06-22 16:42:28 +0900

    feat: 유클리드 호제법으로 최대공약수 구하기
    ## 백준 2609
    ## 최대공약수
    - 두 수 A, B를 MOD 연산(%)으로 소인수분해
    - B가 0이 될때까지 재귀 호출, B가 0이면 A가 최대공약수

--- src/main/java/harucoding/numbertheory/GCDLCM.java
+++ src/main/java/harucoding/numbertheory/GCDLCM.java
@@ -13,5 +13,15 @@ public class GCDLCM {
 
         int A = Integer.parseInt(str.nextToken());
         int B = Integer.parseInt(str.nextToken());
+
+        int result = GCD(A, B);
+    }
+
+    private static int GCD(int a, int b) { // 유클리드 호제법으로 최대공약수 구하기
+        if (b == 0) { // B가 0일 때 A가 최대공약수
+            return a;
+        } else {
+            return GCD(b, a % b); // 재귀 형식
+        }
     }
 }
