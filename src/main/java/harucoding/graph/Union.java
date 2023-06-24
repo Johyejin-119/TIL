29f5c7190267e91aa18deb0b863cd3f8b6c955de
Johyejin-119 <hyejinjo0101@gmail.com>
2023-06-24 21:16:21 +0900

    feat: 질의 유형에 따라 0이면 union() 연산을 수행
    ## 백준 1717

--- src/main/java/harucoding/graph/Union.java
+++ src/main/java/harucoding/graph/Union.java
@@ -16,5 +16,17 @@ public class Union {
         for (int i = 0; i < N + 1; i++) {
             node[i] = i;
         }
+        for (int i = 0; i < M; i++) { // 질의 계산
+            str = new StringTokenizer(br.readLine());
+            int type = Integer.parseInt(str.nextToken()); // 질의 유형
+            int a = Integer.parseInt(str.nextToken()); // 노드
+            int b = Integer.parseInt(str.nextToken()); // 노드
+
+            if (type == 0) { // 합집합 union
+                union(a, b);
+            } else {
+
+            }
+        }
     }
 }
