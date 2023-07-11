2747ee296d0eee45e6fa4e670d9e9dcb49080bfe
Johyejin-119 <hyejinjo0101@gmail.com>
2023-07-11 16:05:54 +0900

    feat: 최단 경로를 구하기 위한 배열과 리스트 선언
    ## 백준 1753 다익스트라

--- src/main/java/harucoding/graph/ShortestRoute.java
+++ src/main/java/harucoding/graph/ShortestRoute.java
@@ -3,19 +3,27 @@ package harucoding.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 // 1753
 public class ShortestRoute {
+    static ArrayList<ArrayList<Node>> gList; // 인접 리스트
+    static int distance[]; // 최단 거리 배열
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer str = new StringTokenizer(br.readLine(), " ");
 
         int V = Integer.parseInt(str.nextToken()); // 정점 개수
         int E = Integer.parseInt(str.nextToken()); // 간선 개수
+        int K = Integer.parseInt(br.readLine()); // 정점 번호
+        int INF = Integer.MAX_VALUE;
+        gList = new ArrayList<>();
+        distance = new int[V + 1];
 
-        str = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(str.nextToken()); // 정점 번호
+        for (int i = 1; i <= V; i++) { // 인접 리스트 초기화
+            gList.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < E; i++) {
             str = new StringTokenizer(br.readLine(), " ");
@@ -27,4 +35,5 @@ public class ShortestRoute {
 
 
     }
+
 }
