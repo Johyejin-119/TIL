b16d5f98f8b4ca7623ae48a55de4c2fdb1b8977c
Johyejin-119 <hyejinjo0101@gmail.com>
2023-07-12 15:26:49 +0900

    feat: 에지 정보 리스트와 최단 거리 배열 초기화
    ## 백준 1865 벨만포드

--- src/main/java/harucoding/graph/Wormhole.java
+++ src/main/java/harucoding/graph/Wormhole.java
@@ -1,16 +1,38 @@
 package harucoding.graph;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 // 1865
 public class Wormhole {
     static ArrayList<ArrayList<Node>> gData;    // 에지 정보 리스트
     static int dist[];                          // 최단 거리 배열
-    static final int INF = Integer.MAX_VALUE;   // 최단 거리 배열 초기값
     static int N, M, W;                         // 노드 수, 도로 수, 웜홀 수
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int TC = Integer.parseInt(br.readLine()); // 테스트 케이스 수
+        StringTokenizer str;
+        for (int i = 0; i < TC; i++) {
+            str = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(str.nextToken()); // 노드 수
+            M = Integer.parseInt(str.nextToken()); // 에지 수
+            W = Integer.parseInt(str.nextToken()); // 에지 수(음수 존재)
+
+            for (int j = 0; j <= N ; j++) { // 에지 정보 리스트 초기화
+                gData.add(new ArrayList<>());
+            }
+
+            int INF = Integer.MAX_VALUE;
+            Arrays.fill(dist, INF); // 최단 거리 배열 초기화
+
+
+        }
     }
 
     private static class Node {
