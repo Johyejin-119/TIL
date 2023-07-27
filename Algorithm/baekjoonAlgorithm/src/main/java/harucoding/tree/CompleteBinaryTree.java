645c9a0165bcc58d2a4c9378cee4e661d052e162
Johyejin-119 <hyejinjo0101@gmail.com>
2023-07-27 19:55:03 +0900

    feat: 완전 이진 트리 탐색, 재귀 함수로 구현
    ## 백준 9934
    ## solve()
    - 최상단 레벨에서 자식 노드순으로 재귀 탐색
    - 시작-끝 노드의 중간 값(m)을 같은 레벨의 list 에 저장

--- Algorithm/baekjoonAlgorithm/src/main/java/harucoding/tree/CompleteBinaryTree.java
+++ Algorithm/baekjoonAlgorithm/src/main/java/harucoding/tree/CompleteBinaryTree.java
@@ -26,6 +26,15 @@ public class CompleteBinaryTree {
         for (int i = 0; i < k; i++) { // 트리 높이 만큼 리스트 초기화
             list.add(new ArrayList<>());
         }
+        solve(0, treeArr.length - 1, 0); // 완전 이진 트리 탐색
+    }
 
+    private static void solve(int s, int e, int depth) {
+        if (depth == k) return; // 최상단 트리 레벨까지 탐색 완료
+        int m = (s + e) / 2;
+        list.get(depth).add(treeArr[m]); // 레벨별 노드 삽입
+
+        solve(s, m - 1, depth + 1); // 하위 레벨, 왼쪽 자식 노드 재귀 탐색
+        solve(m + 1, e, depth + 1); // 하위 레벨, 오른쪽 자식 노드 재귀 탐색
     }
 }
